93109093b7edd40f475c20ad9e6a2c12cc9d9d25
Glitch (hello-express) <none>
2020-11-02 18:04:40 +0000

    🔮🍢 Checkpoint ./server.js:19656024/36314

--- server.js
+++ server.js
@@ -1,164 +1,300 @@
-//coded by : Ra3d🍁#0001 - Ln,Muziky#8956 - ! - Rè • F O R E V ER#0002
-const http = require("http");
-const express = require("express");
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-
-//coded by : Ra3d🍁#0001 - Ln,Muziky#8956
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const { Client, RichEmbed } = require("discord.js");
 
 client.on("ready", () => {
-  console.log(`Logged in as : ${client.user.username}`);
-  client.user.setActivity(`${prefix}Help`);
-  client.user.setStatus("idle");
+  console.log(`I Am Ready!!`);
 });
+var prefix = "1";
 
-//coded by : Ra3d🍁#0001 - Ln,Muziky#8956 -! - Rè • F O R E V ER#0002
-
-var prefix = "-"; // تعديل مهم جدا
-
+const owner = [
+  "670670491828224000",
+  "721370985105719346",
+  "734136447832948817",
+  ""
+];
 client.on("message", message => {
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
   if (message.content.startsWith(prefix + "bc")) {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    let args = message.content
-      .split(" ")
-      .slice(1)
-      .join(" ");
-    if (!args) return message.reply(`Please Type message bc`);
-    message.guild.members
-      .filter(m => m.presence.status !== "online")
-      .forEach(m => {
-        m.send(`${args}\n ${m}`)
-          .then(() => {
-            console.log(`Send To ${m.user.tag} Successfully!`);
-          })
-          .catch(function() {
-            console.log("i cant send to: " + m.user.tag + ".");
-          });
-      });
+    if (!owner.includes(message.author.id))
+      return message.reply("**6ix9ine Only:x:**");
+    message.delete();
     let embed = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setColor("#5016f3")
-      .setDescription(
-        `\`${
-          message.guild.members.filter(m => m.presence.status !== "online").size
-        }\` : تمكن ${client.user} من الارسال الى `
+      .setColor(`RANDOM`)
+      .setDescription(`**! اكتب رسالة لأرسال البرودكاست**`)
+      .addField(`**${prefix}bc [Bc Message]**`, `${message.author}`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp()
+      .setThumbnail(message.author.avatarURL);
+    if (!args) return message.channel.sendEmbed(embed);
+    message.guild.members.forEach(m => {
+      m.send(`${args}\n ${m}`);
+      message.delete();
+    });
+    const embedd = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .addField(
+        "**! تم ارسال برودكاست الي كل الاعضاء**",
+        `**-----------------------**`
       )
+      .setThumbnail(message.author.avatarURL)
+      .addField(`**Message :**`, `${args}`)
+      .addField("**By :**", `**${message.author}**`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
       .setTimestamp();
-    message.channel.send({ embed: embed }); //coded by : Ra3d🍁#0001 - Ln,Muziky#8956
-
+    message.channel.sendEmbed(embedd);
     message.delete();
   }
 });
-
-//كود برودكاست للاون لاين فقط
 
 client.on("message", message => {
   if (message.content.startsWith(prefix + "obc")) {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    let args = message.content
-      .split(" ")
-      .slice(1)
-      .join(" ");
-    if (!args) return message.reply(`Please Type message bc`);
+    if (!owner.includes(message.author.id))
+      return message.reply("**6ix9ine Only:x:**");
+    message.delete();
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(" ");
+    let embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+      .setDescription(`**! اكتب رسالة لأرسال البرودكاست**`)
+      .addField(`**${prefix}obc [Bc Message]**`, `${message.author}`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp()
+      .setThumbnail(message.author.avatarURL);
+    if (!argresult) return message.channel.sendEmbed(embed);
     message.guild.members
       .filter(m => m.presence.status !== "offline")
       .forEach(m => {
-        m.send(`${args}\n ${m}`)
-          .then(() => {
-            console.log(`Send To ${m.user.tag} Successfully!`);
-          })
-          .catch(function() {
-            console.log("i cant send to: " + m.user.tag + ".");
-          });
+        m.send(`${argresult}\n ${m}`);
+        message.delete();
       });
-    let embed = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setColor("#5016f3")
-      .setDescription(
-        `\`${
-          message.guild.members.filter(m => m.presence.status !== "online").size
-        }\` : تمكن ${client.user} من الارسال الى `
+    const embeddd = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .addField(
+        "**! تم ارسال برودكاست الي الاعضاء الاونلاين**",
+        `**-----------------------**`
       )
+      .setThumbnail(message.author.avatarURL)
+      .addField(`**Message :**`, `${argresult}`)
+      .addField("**By :**", `**${message.author}**`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
       .setTimestamp();
-    message.channel.send({ embed: embed }); //coded by : Ra3d🍁#0001 - Ln,Muziky#8956
-
+    message.channel.sendEmbed(embeddd);
     message.delete();
   }
 });
-//coded by : Ra3d🍁#0001 - Ln,Muziky#8956
 
-//coded by : Ra3d🍁#0001 - Ln,Muziky#8956
-
-client.on("message", message => {
-  if (
-    message.content.toLowerCase() === prefix + "رعد".toLowerCase() ||
-    message.content.toLowerCase() === prefix + "ra3d".toLowerCase() ||
-    message.content.toLowerCase() === prefix + "yt".toLowerCase()
-  ) {
-    var invite = new Discord.RichEmbed()
-      .setTimestamp()
-      .setColor(`#141414`)
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setFooter(`${client.user.tag} BOT `)
-      .setDescription(`**:candle: شكرا ااختيارك قناه رعد استمتع!! 
-[Channel Link](https://www.youtube.com/channel/UCAVB8JOSy_y3qoR7bIsiAYg) .**`);
-    message.author
-      .send(invite)
-      .then(m => {
-        message.react("✅");
-      })
-      .catch(err => {
-        message.react("❌");
-      });
-  }
-});
-
-client.on("message", message => {
-  if (message.content == prefix + "help") {
+client.on("message", async message => {
+  if (message.content.startsWith(prefix + "help")) {
+    if (!owner.includes(message.author.id))
+      return message.reply("**6ix9ine Only:x:**");
+    message.delete();
     let help = new Discord.RichEmbed()
-      .setColor("0x5016f3")
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(`** ${client.user} Commands :**
-
-**BoardCast**
-\`${prefix}bc\` **| bc to all! [Upgrade] -New can send to bots-**
-\`${prefix}obc\` ** | bc to all online! [Upgrade] -New can send to bots-**
-\`${prefix}inv\` ** | to invite bot in your server! : )**
-\`${prefix}support\`  ** | to get an invite link for the support server! : )**
-
-**Code Developers : <@734136447832948817> - <@609544947816988702>**
-            `);
-    message.channel.send(help);
+      .setColor("RANDOM")
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp()
+      .setThumbnail(`${message.author.avatarURL}`)
+      .setDescription(
+        `**-------------------------
+          ${prefix}bc : لـ ارسال برودكاست الي الكل
+          ${prefix}obc : لـ ارسال برودكاست الي الاونلاين
+          -------------------------
+          ${prefix}setName : لـ تـغـيـر الأسـم
+          ${prefix}setAvatar : لـ تـغـيـر الـصـورة
+          -------------------------
+          ${prefix}invite : لـ دعوة الـبـوت
+          ${prefix}bot : لـ معلومات البوت
+          -------------------------**`
+      )
+      .addField(`**By :**`, `**<@670670491828224000>**`);
+    message.channel.sendEmbed(help);
+    message.delete();
   }
 });
-//coded by : Ra3d🍁#0001 - Ln,Muziky#8956
 
-//coded by : Ra3d🍁#0001 - Ln,Muziky#8956
+const change = "734136447832948817";
+client.on("message", message => {
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (message.content.startsWith(prefix + "setName")) {
+    if (!change.includes(message.author.id))
+      return message.reply("**6ix9ine Only:x:**");
+    message.delete();
+    let embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+      .setDescription(`**! اكتب اسم لتغير الاسم القديم**`)
+      .addField(`**${prefix}setName [New Name]**`, `${message.author}`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp()
+      .setThumbnail(message.author.avatarURL);
+    if (!args) return message.channel.sendEmbed(embed);
+    client.user.setUsername(`${args}`);
+    const name = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`**! تـم تـغـيـر الأسـم الـي : ${args}**`)
+      .addField(`**By :**`, `**${message.author}**`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp();
+    message.channel.sendEmbed(name);
+  }
+});
+client.on("message", message => {
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (message.content.startsWith(prefix + "setname")) {
+    if (!change.includes(message.author.id))
+      return message.reply("**6ix9ine Only:x:**");
+    message.delete();
+    let embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+      .setDescription(`**! اكتب اسم لتغير الاسم القديم**`)
+      .addField(`**${prefix}setname [New Name]**`, `${message.author}`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp()
+      .setThumbnail(message.author.avatarURL);
+    if (!args) return message.channel.sendEmbed(embed);
+    client.user.setUsername(`${args}`);
+    const name = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`**! تـم تـغـيـر الأسـم الـي : ${args}**`)
+      .addField(`**By :**`, `**${message.author}**`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp();
+    message.channel.sendEmbed(name);
+  }
+});
 
 client.on("message", message => {
-  var prefix = "-"; ///coded by : Ra3d🍁#0001 - Ln,Muziky#8956
-
-  if (!message.channel.guild) return; //coded by : Ra3d🍁#0001 - Ln,Muziky#8956
-
-  if (message.content.startsWith(prefix + "ping")) {
-    if (!message.channel.guild) return;
-    var msg = `${Date.now() - message.createdTimestamp}`;
-    var api = `${Math.round(client.ping)}`;
-    if (message.author.bot) return; //coded by : Ra3d🍁#0001 - Ln,Muziky#8956
-
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (message.content.startsWith(prefix + "setAvatar")) {
+    if (!change.includes(message.author.id))
+      return message.reply("**6ix9ine Only:x:**");
+    message.delete();
     let embed = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setColor("#5016f3")
-      .addField("**My Ping is:**", msg + " ms :signal_strength: ")
-      .addField("**Discord API :**", api + " ms :signal_strength: ")
+      .setColor(`RANDOM`)
+      .setDescription(`**! ضع رابط الصورة لتغير الصورة**`)
+      .addField(`**${prefix}setAvatar [Avatar Link]**`, `${message.author}`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp()
+      .setThumbnail(message.author.avatarURL);
+    if (!args) return message.channel.sendEmbed(embed);
+    client.user.setAvatar(`${args}`);
+    const avatar = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`**! تـم تـغـيـر الـصـورة**`)
+      .addField(`**By :**`, `**${message.author}**`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
       .setTimestamp();
-    message.channel.send({ embed: embed }); //coded by : Ra3d🍁#0001 - Ln,Muziky#8956
+    message.channel.sendEmbed(avatar);
   }
 });
-//coded by : Ra3d🍁#0001 - Ln,Muziky#8956
+client.on("message", message => {
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (message.content.startsWith(prefix + "setavatar")) {
+    if (!change.includes(message.author.id))
+      return message.reply("**6ix9ine Only:x:**");
+    message.delete();
+    let embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+      .setDescription(`**! ضع رابط الصورة لتغير الصورة**`)
+      .addField(`**${prefix}setavatar [Avatar Link]**`, `${message.author}`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp()
+      .setThumbnail(message.author.avatarURL);
+    if (!args) return message.channel.sendEmbed(embed);
+    client.user.setAvatar(`${args}`);
+    const avatar = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`**! تـم تـغـيـر الـصـورة**`)
+      .addField(`**By :**`, `**${message.author}**`)
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp();
+    message.channel.sendEmbed(avatar);
+  }
+});
 
-client.login(process.env.token);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on("ready", () => {
+  client.user.setStatus("online");
+});
+//client.on("ready", () => {
+//    client.user.setActivity(`6ix9ine Only.`, {
+//        type: "playing"
+//    });
+//});
+
+var statuss = ["", ``];
+var secound = 10;
+client.on("ready", () => {
+  var timeing = Math.floor(secound * 1000);
+  setInterval(function() {
+    var ammount = statuss.length;
+    var num = Math.floor(Math.random() * ammount);
+    client.user.setActivity(statuss[num], { type: "" });
+  }, timeing);
+});
+
+client.on("message", message => {
+  if (message.content === prefix + "invite") {
+    var SUPPORT =
+      "https://discord.com/api/oauth2/authorize?client_id=705043813839667200&permissions=0&scope=bot";
+    let embed = new Discord.RichEmbed()
+      .setTitle(`Invite Link`)
+      .setDescription(
+        `                                                                                                                 
+**[ Invite Link ](${SUPPORT})**`
+      )
+      .setColor("RANDOM")
+      .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+      .setTimestamp()
+      .setThumbnail(`${message.author.avatarURL}`)
+      .addField(`**By :**`, `${message.author}`);
+    message.react("?");
+    message.channel.send(embed);
+  }
+});
+
+client.on("message", message => {
+  if (message.content.startsWith(prefix + "bot")) {
+    message.channel.send({
+      embed: new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle("**[ Broad Cast ]**")
+        .addField("``Bot Name :``", `[ ${client.user.tag} ]`)
+        .addField("``Bot ID :``", `[ ${client.user.id} ]`)
+        .addField("``Servers :``", `[ ${client.guilds.size} ]`)
+        .addField("``Channels :``", `[ ${client.channels.size} ]`)
+        .addField("``Users :``", `[ ${client.users.size} ]`)
+        .addField("``Bot Prefix :``", `[ ${prefix} ]`)
+        .addField("``Owner :``", `<@670670491828224000>`)
+        .setThumbnail(client.user.avatarURL)
+        .setTimestamp()
+        .setAuthor(client.user.tag, client.user.avatarURL)
+        .setFooter(`${message.author.tag}`, `${message.author.avatarURL}`)
+    });
+  }
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.isMentioned(client.user)) {
+    message.reply(`**Bot Prefix** : [ ${prefix} ]`);
+  }
+});
+
+client.login("NzM1ODgwNjIwODEwNjk4ODEy.XxmsPA.BW8ygSfT2ZjginUqIJlnLmIgCQs");
